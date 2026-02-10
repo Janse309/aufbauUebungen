22a057a1b08c70e0b6db46d2f81b22d687657231
janse <jansimoboecker40@gmail.com>
2026-02-10 16:32:13 +0100

    html-templates.css and html-templates.js added

---
+++ html-templates.js
@@ -0,0 +1,65 @@
+ let name = 'Frederick'; // Für Aufgabe 4
+
+
+        /**
+         * Aufgabe 1 
+         */
+        function joke1() {
+            document.getElementById('joke').innerHTML =
+                `Wie viele Windows-Anwender braucht man um eine Glühbirne zu wechseln? 100! Einer wechselt die Birne, 99 klicken die Fehlermeldungen weg.`;
+        }
+
+
+        /**
+         * Aufgabe 1 
+         */
+        function joke2() {
+            document.getElementById('joke').innerHTML =
+                `Linux wird nie das meistinstallierte Betriebssystem sein, wenn man bedenkt, wie oft man Windows neu installieren muss!`;
+        }
+
+        /**
+         * Aufgabe 2 
+         */
+        function addFruit(fruit) {
+            document.getElementById('food').innerHTML += `Frucht hinzugefügt: ${fruit} <br>`;
+        }
+
+        function deleteFruits() {
+            document.getElementById('food').innerHTML = '';
+        }
+
+
+        /**
+         * Aufgabe 3 
+         */
+        function generatedCircle() {
+            document.getElementById('generatedHTML').innerHTML = `
+            <div class="circle">
+                Kreis    
+            </div>
+            `;
+        }
+
+        /**
+         * Aufgabe 4 (Hilfsfunktion um den Namen zu ändern) 
+         */
+        function setName(n) {
+            name = n;
+        }
+
+
+
+        /**
+         * Aufgabe 5 
+         */
+        function sendMessage() {
+            let message = document.getElementById('message'); // HTML Element mit ID message wird an die Variable 'message' zugewiesen. 
+            let chat = document.getElementById('chat');
+
+            chat.innerHTML += `
+            <div>Unbekannt: ${message.value}</div>
+            `;
+
+            message.value = ''; // Inhalt von Textfeld mit id "message" löschen
+        }
